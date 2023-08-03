ebb2200b8403cb9cd34ee6b525978c36249d0d7c
Damon <126731021+damon314159@users.noreply.github.com>
2023-08-03 16:25:26 +0100

    Setup meta files for webpack, eslint, prettier

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      loader: 'html-loader',
+      filename: 'index.html',
+      template: './src/html/index.template.html',
+      title: 'To Do'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource'
+      },
+      {
+        test: /\.html$/i,
+        loader: 'html-loader'
+      }
+    ]
+  },
+  devtool: 'inline-source-map'
+}
