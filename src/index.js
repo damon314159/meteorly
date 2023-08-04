4bcf61af5f065cb512191d17b96dde681f7714b9
Damon <126731021+damon314159@users.noreply.github.com>
2023-08-04 22:57:28 +0100

    Format weekly and daily forecast data objects
    These are in console still for now, until a UI is built

--- src/index.js
+++ src/index.js
@@ -1,20 +1,28 @@
 require('./css/style.css')
 
-async function requestWeatherAPI(location) {
-  const publicKey = '3c7d101d6aad41f39b9163700230208'
-  const url = `https://api.weatherapi.com/v1/forecast.json?key=${publicKey}&q=${location}&days=7`
-  const response = await fetch(url)
-  const weatherData = await response.json()
-  forecastWeek(weatherData)
+async function forecastDay(data) {
+  const hour = new Date().getHours()
+  const [today, tomorrow] = await data.forecast.forecastday.slice(0, 2)
+  const todayHourData = today.hour.slice(hour)
+  const tomorrowHourData = tomorrow.hour.slice(0, (hour + 1) % 24)
+  const hourData = []
+  todayHourData.concat(tomorrowHourData).forEach((hourObj) =>
+    hourData.push({
+      time: hourObj.time.slice(11), // hh:00
+      temp: hourObj.temp_c,
+      rainChance: hourObj.chance_of_rain,
+      condition: hourObj.condition
+    })
+  )
+  return hourData
 }
-window.requestWeatherAPI = requestWeatherAPI
 
 async function forecastWeek(data) {
   const allDays = await data.forecast.forecastday
   const dayData = []
   allDays.forEach((dayObj) =>
     dayData.push({
-      date: dayObj.date,
+      date: dayObj.date.slice(5), // mm-dd
       avgTemp: dayObj.day.avgtemp_c,
       maxTemp: dayObj.day.maxtemp_c,
       minTemp: dayObj.day.mintemp_c,
@@ -23,6 +31,15 @@ async function forecastWeek(data) {
       condition: dayObj.day.condition
     })
   )
-  console.log(dayData)
   return dayData
 }
+
+async function requestWeatherAPI(location = 'London') {
+  const publicKey = '3c7d101d6aad41f39b9163700230208'
+  const url = `https://api.weatherapi.com/v1/forecast.json?key=${publicKey}&q=${location}&days=7&aqi=no&alerts=no`
+  const response = await fetch(url, { mode: 'cors' })
+  const weatherData = await response.json()
+  console.log(await forecastWeek(weatherData)) // temp
+  console.log(await forecastDay(weatherData)) // temp
+}
+window.requestWeatherAPI = requestWeatherAPI // temp
