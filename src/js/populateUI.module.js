7071f0a1296cf6fe6fadd2a65ff71d4707894cc3
Damon <126731021+damon314159@users.noreply.github.com>
2023-08-06 23:23:45 +0100

    Split UI population into module, with icon loading

---
+++ src/js/populateUI.module.js
@@ -0,0 +1,65 @@
+export default async function populateUI(conditionDict, requestWeatherAPI, query) {
+  const loadImage = (imageName) => import(`../assets/icons/${imageName}.png`)
+
+  function fillSpans(nodes, data) {
+    nodes.forEach((node) => {
+      const el = node
+      if (data[el.dataset.contains] !== undefined) {
+        el.textContent = data[el.dataset.contains]
+      }
+    })
+  }
+
+  function fillCondition(el, code) {
+    const img = el
+    loadImage(conditionDict[code].icon).then((module) => {
+      img.src = module.default
+    })
+  }
+
+  function populateLocation(data) {
+    const locationSpan = document.querySelector('.location span')
+    if (data.region) {
+      locationSpan.textContent = `${data.city}, ${data.region}`
+    } else {
+      locationSpan.textContent = `${data.city}, ${data.country}`
+    }
+  }
+
+  function populateOverview(data) {
+    const overview = document.querySelector('.overview')
+    const spans = overview.querySelectorAll('span')
+    const img = overview.querySelector('.condition-icon')
+    fillSpans(spans, data)
+    fillCondition(img, data.condition.code)
+  }
+
+  function populateDay(data) {
+    const hours = document.querySelectorAll('.hour')
+    for (let i = 0; i < data.length; i += 1) {
+      const spans = hours[i].querySelectorAll('span')
+      const img = hours[i].querySelector('.condition-icon')
+      fillSpans(spans, data[i])
+      fillCondition(img, data[i].condition.code)
+    }
+  }
+
+  function populateWeek(data) {
+    const days = document.querySelectorAll('.day')
+    for (let i = 0; i < data.length; i += 1) {
+      const spans = days[i].querySelectorAll('span')
+      const img = days[i].querySelector('.condition-icon')
+      fillSpans(spans, data[i])
+      fillCondition(img, data[i].condition.code)
+    }
+  }
+
+  const requestObj = await requestWeatherAPI(query)
+  Promise.all(Array.from(Object.values(requestObj))).then((values) => {
+    const [location, overview, day, week] = values
+    populateLocation(location)
+    populateOverview(overview)
+    populateDay(day)
+    populateWeek(week)
+  })
+}
